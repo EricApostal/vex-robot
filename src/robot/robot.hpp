b421237f0dffe4475008358b057ec7dce4a8f5ed
EricApostal <60072374+EricApostal@users.noreply.github.com>
2023-09-14 16:59:36 -0400

    updating for OOP jazz

---
+++ src/robot/robot.hpp
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "motors.hpp"
+#include "tasks.hpp"
+
+using namespace pros;
+
+class Robot {
+    public:
+        /*
+            0 = idle
+            1 = running
+            2 = completed
+        */
+        int status = 0;
+        Tasks task = Tasks(asd);
+};
---
+++ src/robot/tasks.hpp
@@ -0,0 +1,22 @@
+/*
+    Task-based system for robot module controls
+*/
+
+using namespace std;
+
+class Tasks {
+    public: 
+        void start() {
+
+        };
+
+        void complete() {
+
+        };
+
+        Tasks( void (&onStart)(), void (&onComplete)() ) {
+            
+        };
+
+
+};
