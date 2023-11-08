8e2178189e09ce27a0c01c04d0fa776657766b66
Eric Apostal <ehapostal04@gmail.com>
2023-11-08 07:15:08 -0500

    All works, currently used in production.

--- src/robot/maps.hpp
+++ src/robot/maps.hpp
@@ -2,21 +2,21 @@
 #include "main.h"
 
 // Drive Motors
-pros::Motor left1 = pros::Motor(-10);
-pros::Motor left2 = pros::Motor(-9);
-pros::Motor left3 = pros::Motor(-7);
+pros::Motor left1 = pros::Motor(-9);
+pros::Motor left2 = pros::Motor(-7);
 
-pros::Motor right1 = pros::Motor(1);
-pros::Motor right2 = pros::Motor(12);
-pros::Motor right3 = pros::Motor(13);
+pros::Motor right1 = pros::Motor(12);
+pros::Motor right2 = pros::Motor(13);
 
-pros::ADIDigitalOut left_bumper(1);
-pros::ADIDigitalOut right_bumper(2);
+pros::ADIDigitalOut left_bumper(7);
+pros::ADIDigitalOut right_bumper(8);
 
-pros::Motor flywheel(8, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor flywheel_1(8, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor flywheel_2(20, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_DEGREES);
+
 pros::Motor arm1(2, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
 
-pros::Motor intake_1(3, pros::E_MOTOR_GEARSET_INVALID, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor intake_1(14, pros::E_MOTOR_GEARSET_INVALID, false, pros::E_MOTOR_ENCODER_DEGREES);
 pros::Motor intake_2(5, pros::E_MOTOR_GEARSET_INVALID, false, pros::E_MOTOR_ENCODER_DEGREES);
 
 pros::Controller controller (pros::E_CONTROLLER_MASTER);
--- src/robot/robot.hpp
+++ src/robot/robot.hpp
@@ -15,11 +15,15 @@ public:
     bool autonStarted = false;
     int intakeState = 0; // -1 puncher, 0 = off, 1 = shooter
     bool arcadeDrive = true;
-
+    bool bumpersOut = false;
 
     Robot()
     {
         arm1.set_brake_mode(MOTOR_BRAKE_HOLD);
+        left1.set_brake_mode(MOTOR_BRAKE_HOLD);
+        left2.set_brake_mode(MOTOR_BRAKE_HOLD);
+        right1.set_brake_mode(MOTOR_BRAKE_HOLD);
+        right2.set_brake_mode(MOTOR_BRAKE_HOLD);
     }
 
     void onTick() override
@@ -28,14 +32,14 @@ public:
         if (mode == 0)
         {
             // drive motors
-            int Left;
-            int Right;
+            int Left = 0;
+            int Right = 0;
             if (arcadeDrive) {
                 int power = controller.get_analog(E_CONTROLLER_ANALOG_LEFT_Y);
                 int turn = controller.get_analog(E_CONTROLLER_ANALOG_RIGHT_X);
 
-                int Left = power + turn;
-                int Right = power - turn;
+                Left = power + turn;
+                Right = power - turn;
             } else {
                 Left = controller.get_analog(E_CONTROLLER_ANALOG_LEFT_Y);
                 Right = controller.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y);
@@ -43,28 +47,9 @@ public:
 
             left1.move(Left);
             left2.move(Left);
-            left3.move(Left);
 
             right1.move(Right);
             right2.move(Right);
-            right3.move(Right);
-
-            // pnuematic pushers
-            // if (controller.get_digital(E_CONTROLLER_DIGITAL_R2))
-            // {
-            //     right_bumper.set_value(1);
-            // }
-            // else
-            // {
-            //     right_bumper.set_value(0);
-            // }
-
-            // if (controller.get_digital(E_CONTROLLER_DIGITAL_L2))
-            // {
-            //     left_bumper.set_value(1);
-            // } else {
-            //     left_bumper.set_value(0);
-            // }
 
 
             if (controller.get_digital(E_CONTROLLER_DIGITAL_R1))
@@ -96,35 +81,55 @@ public:
             {
                 if (intakeState == 0)
                 {
-                    flywheel.move(-127);
+                    flywheel_1.move(-127);
+                    flywheel_2.move(127);
                     intakeState = 1;
                 }
                 else
                 {
-                    flywheel.move(0);
+                    flywheel_1.move(0);
+                    flywheel_2.move(0);
                     intakeState = 0;
                 }
             }
-            else if (controller.get_digital_new_press(E_CONTROLLER_DIGITAL_B))
+            else if (controller.get_digital_new_press(E_CONTROLLER_DIGITAL_A))
             {
                 if (intakeState == 0)
                 {
-                    flywheel.move(127);
+                    flywheel_1.move(127);
+                    flywheel_2.move(-127);
                     intakeState = -1;
                 }
                 else
                 {
-                    flywheel.move(0);
+                    flywheel_1.move(0);
+                    flywheel_2.move(0);
                     intakeState = 0;
                 }
-            } else if (controller.get_digital_new_press(E_CONTROLLER_DIGITAL_DOWN)) {
+            } else if (controller.get_digital_new_press(E_CONTROLLER_DIGITAL_LEFT)) {
                 if (intakeState == 0) {
-                flywheel.move(-60);
-                intakeState = 2;
+                    flywheel_1.move(-60);
+                    flywheel_2.move(60);
+                    intakeState = 2;
                 } else {
-                    flywheel.move(0);
+                    flywheel_1.move(0);
+                    flywheel_2.move(0);
                     intakeState = 0;
                 }
+            }
+            if (controller.get_digital_new_press(E_CONTROLLER_DIGITAL_X)) {
+                arcadeDrive = !arcadeDrive;
+            }
+            if (controller.get_digital_new_press(E_CONTROLLER_DIGITAL_B)) {
+                bumpersOut = !bumpersOut;
+            }
+
+            if (bumpersOut) {
+                left_bumper.set_value(1);
+                right_bumper.set_value(1);
+            } else {
+                left_bumper.set_value(0);
+                right_bumper.set_value(0);
             }
         }
         else if (mode == 1)
