d07ffee8bf4f7d8bc0ff099930a35faf9f40875e
EricApostal <60072374+EricApostal@users.noreply.github.com>
2023-12-06 18:33:38 -0500

    Did some auton stuff

--- src/robot/auton/one.hpp
+++ src/robot/auton/one.hpp
@@ -84,13 +84,13 @@ void doAuton()
     arm.move_relative(-400, 70);
     motors::system::move_flywheel(127);
     motors::system::move_intake(127);
-    chassis.moveTo(-28, -5, 0, 10000, true, 0, 0.1, 75);
+    chassis.moveTo(-28, -5, 0, 10000, true, 0, 0.1, 100);
     pros::delay(250);
     chassis.turnTo(50, 10, 10000);
     pros::delay(500);
     motors::system::move_intake(0);
     arm.move_absolute(0, 706);
-    pros::delay(500);
+    pros::delay(1000);
     motors::system::move_flywheel(-127);
     arm.move_absolute(-1650, 700);
     pros::delay(1000);
@@ -110,7 +110,7 @@ void doAuton()
     pros::delay(500);
     chassis.turnTo(-60, -60, 10000);
     pros::delay(500);
-    chassis.moveTo(5, -62, 0, 10000, false, 0, 0.1, 75);
+    chassis.moveTo(5, -62, 270, 10000, false, 0, 0.1, 75);
     pros::delay(1000);
     // chassis.turnTo(-60, -40, 10000);
     // pros::delay(1000);
