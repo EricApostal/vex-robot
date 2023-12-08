2fd59a999d4a0fc3b26d113f9142dbc4f6ce0506
EricApostal <60072374+EricApostal@users.noreply.github.com>
2023-12-08 13:15:42 -0500

    bruh skills

--- src/robot/auton/skills.hpp
+++ src/robot/auton/skills.hpp
@@ -13,9 +13,6 @@ void doSkills()
 {
     chassis.setPose(-41, -62, 0);
     arm.move_relative(-850, 127);
-    chassis.moveToPose(-46.5, -46.5, 0, 10000, false, 0, 0, 50, false);
-    pros::delay(1000);
-    // chassis.turnTo(47, 0, 10000, true, 50, false);
-    chassis.moveToPose(-65, -65, 0, 10000, true, 0, 0.6, 50, false);
+    chassis.moveToPose(-42, -49, 0, 10000, false, 0, 0, 50, false);
     pros::delay(10000);
 }
