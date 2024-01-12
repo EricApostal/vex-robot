06adc97224ba11aa513cd3cf58b36cf0d4dfcc73
EricApostal <60072374+EricApostal@users.noreply.github.com>
2024-01-12 17:02:26 -0500

    close side auton

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "robot/robot.hpp"
 #include "lib/scheduler.hpp"
 #include "lib/tasks.hpp"
-#include "robot/auton/far_side.hpp"
+#include "robot/auton/close_side.hpp"
 #include "robot/auton/skills.hpp"
 #include "robot/chassis.hpp"
 
@@ -12,6 +12,9 @@ Scheduler scheduler;
 Robot bot = Robot();
 
 void initialize() {
+	left_side_motors.set_brake_modes(pros::E_MOTOR_BRAKE_HOLD);
+	right_side_motors.set_brake_modes(pros::E_MOTOR_BRAKE_HOLD);
+	
 	pros::lcd::initialize();
 	pros::Task initLem([&]() {
 		init_chassis();
@@ -23,12 +26,13 @@ void disabled() {}
 void competition_initialize() {}
 
 void autonomous() {
+
 	doAuton();
 }
 
 // Driver control start (always called when not connected to field controller)
 void opcontrol() {
-	// pros::delay(3000);
+	pros::delay(3000);
 	// doAuton();
 	bot.taskName = "robot";
 	scheduler.addTask(&bot);
---
+++ src/robot/auton/close_side.hpp
@@ -0,0 +1,43 @@
+#pragma once
+#include "main.h"
+#include "../../lib/tasks.hpp"
+#include "../../lib/motors_mapped.hpp"
+#include "lemlib/api.hpp"
+#include "../chassis.hpp"
+
+/*
+    Auton testing, useless otherwise. Should be added to scheduler.
+*/
+
+// https://path.jerryio.com/
+
+void doAuton()
+{  
+    left_side_motors.set_brake_modes(pros::E_MOTOR_BRAKE_HOLD);
+	right_side_motors.set_brake_modes(pros::E_MOTOR_BRAKE_HOLD);
+    chassis.setPose(31, -59, 90);
+    chassis.moveToPoint(56, -50, 1000, true, 70);
+    chassis.waitUntilDone();
+    motors::system::move_bumpers(true);
+    pros::delay(500);
+    // 60, -25
+    motors::system::move_bumpers(false);
+    chassis.moveToPoint(70, -22, 1000, true, 85);
+    pros::delay(500);
+    chassis.moveToPoint(47, -47, 1000, false, 50);
+    pros::delay(500);
+    motors::system::move_intake(127);
+    chassis.moveToPoint(20, -18, 2000, 50);
+    pros:delay(500);
+    chassis.moveToPoint(4, 0, 1000);
+    pros::delay(500);
+    motors::system::move_bumpers(true);
+    chassis.turnTo(40, 10, 1000);
+    chassis.moveToPoint(60, 20, 1000, 60);
+    pros::delay(500);
+    motors::system::move_bumpers(false);
+    motors::system::move_intake(-127);
+    pros::delay(500);
+    motors::system::move_intake(0);
+    chassis.waitUntilDone();
+}
